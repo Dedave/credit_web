58c9ab093896edd21131d83b45a9ad489fdccb5f
Dedave <83037005+Dedave@users.noreply.github.com>
2022-07-17 01:01:20 +0100

    Update server.js

--- web/server.js
+++ web/server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/credit_web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/credit_web/web/index.html'));
+res.sendFile(path.join(__dirname+'/dist/credit_web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
