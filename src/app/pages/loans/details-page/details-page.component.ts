5b57297e233c2348613a6aa936a786034d21f8de
Dedave <83037005+Dedave@users.noreply.github.com>
2022-07-17 14:19:39 +0100

    Updated details-page

--- src/app/pages/loans/details-page/details-page.component.ts
+++ src/app/pages/loans/details-page/details-page.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-page.component.css']
 })
 export class DetailsPageComponent implements OnInit {
-
+cover=true
+coverr=true
+up=true
+upp=true
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openCover(){
+    this.cover= !this.cover
+    this.up=!this.up
+  }
+  opencover(){
+    this.coverr= !this.coverr
+    this.upp=!this.upp
   }
 
 }
